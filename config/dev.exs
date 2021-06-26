bda67f7bc7faffedccb876a30cde7662cb43acfa
woarewe <rostislav@hey.com>
2021-06-26 16:16:49 +0300

    Setup database and header

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :discuss, Discuss.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "larrys",
   database: "discuss_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,

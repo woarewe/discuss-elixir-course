1942ea2267c6c41498a2c858f3c46e24a71f41c6
woarewe <rostislav@hey.com>
2021-06-27 16:57:22 +0300

    Add ability to delete

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -46,4 +46,12 @@ defmodule DiscussWeb.TopicController do
       {:error, changeset} -> render conn, "edit.html", changeset: changeset, topic: topic
     end
   end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Discuss.Repo.get!(Topic, topic_id) |> Discuss.Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic was deleted")
+    |> redirect(to: Routes.topic_path(conn, :index))
+  end
 end
